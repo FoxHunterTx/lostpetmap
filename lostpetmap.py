73ed91fb4880db7f44dd8a541b05265f6195d301
root <root@webserver-01.c.webserver1-197718.internal>
2018-03-27 23:11:52 +0000

    updates to check input data

--- lostpetmap.py
+++ lostpetmap.py
@@ -1,34 +1,65 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+
 import json
 import dbconfig
-#if dbconfig.test:
-#  from mockdbhelper import MockDBHelper as DBHelper
-#else:
-from dbhelper import DBHelper
+import datetime
+import dateparser
+#from dateutil import parser
+
+
+if dbconfig.test:
+  from mockdbhelper import MockDBHelper as DBHelper
+else:
+  from dbhelper import DBHelper
 
 
 app = Flask(__name__)
 DB = DBHelper()
 
+categories_form = ['cat','dog','other']
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date,"%Y-%m-%d")
+    except TypeError:
+        return None
+
+
 
 @app.route("/")
-def home():
+def home(error_message=None):
    lostpets = DB.get_all_lost_pet()
    lostpets = json.dumps(lostpets)
-   return render_template("home.html",lostpetsdata=lostpets)
+   return render_template("home.html",lostpetsdata=lostpets,categories=categories_form,error_message=error_message)
 
 
 @app.route("/submitlostpet", methods=['POST'])
 def submitlostpet():
-    category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
-    DB.add_pet(category,date,latitude,longitude,description)
-    return home()
+    try:
+        error_message = None
+        category = request.form.get("category")
+        if category not in categories_form:
+          return home()
+
+        #date = request.form.get("date")
+        date = format_date(request.form.get("date"))
+        if not date:
+          return home("Invalid date. Please use yyyy-mm-dd format")
+        try:
+          latitude = float(request.form.get("latitude"))
+          longitude = float(request.form.get("longitude"))
+        except:
+          error_message = "Latitude and Longitude have incorrect format"
+          return home(error_message)
+        description = request.form.get("description")
+        DB.add_pet(category,date,latitude,longitude,description)
+        return home()
+    except Exception as e:
+        print (e)
 
 
 if __name__ == '__main__':
